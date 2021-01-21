ad8a880d7d9c01be836d7086f7e0404fe4eec42c
Dmitrii Pisarenko <d.pisarenko@jit.at>
2021-01-21 18:06:21 +0100

    JO-124: Now sample processes are created at startup.

--- src/main/java/at/jit/incidentlistener/demo/DemoApplication.java
+++ src/main/java/at/jit/incidentlistener/demo/DemoApplication.java
@@ -4,12 +4,14 @@ import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.engine.RepositoryService;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.spring.boot.starter.SpringBootProcessApplication;
-import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.event.EventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @ProcessApplication
@@ -27,6 +29,23 @@ public class DemoApplication extends SpringBootProcessApplication {
     @EventListener
     private void processPostDeploy(final PostDeployEvent evt) {
         System.out.println("X");
-        runtimeService.startProcessInstanceByKey("TestOptimizeProcess");
+
+        for (int i=0; i < 20; i++) {
+            startProcess("1", "1.1", String.format("Path 1.1, instance %d", i+1));
+        }
+        for (int i=0; i < 30; i++) {
+            startProcess("1", "1.2", String.format("Path 1.2, instance %d", i + 1));
+        }
+        for (int i=0; i < 150; i++) {
+            startProcess("2", null, String.format("Path 2, instance %d", i + 1));
+        }
+    }
+
+    private void startProcess(final String path, final String subPath,
+                              final String businessKey) {
+        final Map<String, Object> vars = new HashMap<>();
+        vars.put("path", path);
+        vars.put("subPath", subPath);
+        runtimeService.startProcessInstanceByKey("TestOptimizeProcess", businessKey, vars);
     }
 }
